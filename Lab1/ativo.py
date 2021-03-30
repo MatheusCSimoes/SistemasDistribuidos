4193f1382fb23e4488847ddfabc22391d0cef7d7
MatheusCSimoes <simoes.matheusc@gmail.com>
2021-03-30 12:30:52 -0300

    Alterando para receber e enviar msg em loop
    Alterando ativo.py para enviar msgs em loop até receber mensagem especifica para fechar conexao e alterando passivo.py para reenviar msg recebida como se fosse um servidor echo

--- Lab1/ativo.py
+++ Lab1/ativo.py
@@ -11,14 +11,21 @@ sock = socket.socket() # default: socket.AF_INET, socket.SOCK_STREAM
 # conecta-se com o par passivo
 sock.connect((HOST, PORTA)) 
 
-# envia uma mensagem para o par conectado
-sock.send(b"Ola, sou o lado ativo!")
+print("Para encerrar o programa entre com a mensagem: 'Encarrar conexao'.")
+while True:
+    inputText = input("Entre com a mensagem para o servidor de echo: ") #le input do usuario
+    #caso o usuario entre com a msg 'Encerrar conexao' quebra o loop de envio e leitura
+    if inputText.upper() == 'Encerrar conexao'.upper(): 
+        break
+    
+    # envia uma mensagem para o par conectado
+    sock.send(inputText.encode())
 
-#espera a resposta do par conectado (chamada pode ser BLOQUEANTE)
-msg = sock.recv(1024) # argumento indica a qtde maxima de bytes da mensagem
+    #espera a resposta do par conectado (chamada pode ser BLOQUEANTE)
+    msg = sock.recv(1024) # argumento indica a qtde maxima de bytes da mensagem
 
-# imprime a mensagem recebida
-print(str(msg,  encoding='utf-8'))
+    # imprime a mensagem recebida
+    print('Mensagem recebida do passivo:', str(msg,  encoding='utf-8'))
 
 # encerra a conexao
 sock.close() 
--- Lab1/passivo.py
+++ Lab1/passivo.py
@@ -13,6 +13,7 @@ sock.bind((HOST, PORTA))
 
 # define o limite maximo de conexoes pendentes e coloca-se em modo de espera por conexao
 sock.listen(5) 
+print ('Aguardando conexao')
 
 # aceita a primeira conexao da fila (chamada pode ser BLOQUEANTE)
 novoSock, endereco = sock.accept() # retorna um novo socket e o endereco do par conectado
@@ -22,9 +23,10 @@ while True:
 	# depois de conectar-se, espera uma mensagem (chamada pode ser BLOQUEANTE))
 	msg = novoSock.recv(1024) # argumento indica a qtde maxima de dados
 	if not msg: break 
-	else: print(str(msg,  encoding='utf-8'))
-	# envia mensagem de resposta
-	novoSock.send(b"Ola, sou o lado passivo!") 
+	else: 
+		print('Mensagem recebida do ativo:', str(msg,  encoding='utf-8'))
+		# envia mensagem de echo
+		novoSock.send(msg) 
 
 # fecha o socket da conexao
 novoSock.close() 
